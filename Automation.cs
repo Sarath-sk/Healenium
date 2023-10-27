d56e6a3622900cc8f6d796233f6ff36b60d389e9
unknown <sarathsk758@gmail.com>
2023-10-27 13:18:43 +0530

    HealeniumProject

---
+++ Automation.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Remote;
+using SeleniumProject.Pages;
+
+namespace SeleniumProject
+{
+    public class Automation
+    {
+        //RemoteWebDriver driver;
+        //IWebDriver driver;
+        Login login;
+
+        [SetUp]
+        public void setup()
+        {
+            String nodeURL = "http://localhost:8085";
+
+            EdgeOptions optionsChrome = new EdgeOptions();
+            optionsChrome.AddArguments("--no-sandbox");
+            optionsChrome.AddArguments("--selenium-manager true");
+
+            RemoteWebDriver driverChrome = new RemoteWebDriver(new Uri(nodeURL), optionsChrome);
+            //driver = new RemoteWebDriver(new Uri(""),;
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driverChrome.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            login = new Login(driverChrome);
+        }
+
+
+        [Test(Author ="Ramya", Description ="This testcase launches the google..")]
+        public void Login()
+        {
+            login.loginToStandardUser();
+        }
+
+    }
+}
---
+++ Pages/Login.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Pages
+{
+    internal class Login
+    {
+        IWebDriver driver;
+
+        public Login(IWebDriver dr)
+        {
+            driver = dr;
+            // To initialise page elements
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "user-name")]
+        IWebElement userName;
+
+        [FindsBy(How = How.Name, Using = "password")]
+        IWebElement password;
+
+        [FindsBy(How = How.Name, Using = "login-button")]
+        IWebElement login;
+
+        public void loginToStandardUser() {
+            userName.SendKeys("standard_user");
+            password.SendKeys("secret_sauce");
+            login.Click();
+        }
+
+
+        //locked_out_user
+        public void loginToLockedUser()    
+        {
+            userName.SendKeys("locked_out_user");
+            password.SendKeys("secret_sauce");
+            login.Click();
+        }   
+
+    }
+}
+    
